f4bc84e9ac3e4b6812f6a5b99a66486e40c79a5f
Dario <1419341+dadapa@users.noreply.github.com>
2019-09-02 15:26:38 +0200

    Update deploy_unzipbackupdeploy.sh

--- deploy_unzipbackupdeploy.sh
+++ deploy_unzipbackupdeploy.sh
@@ -1,8 +1,9 @@
-m -rf /var/www/sites/appname/deploy/*.deployed
-unzip /var/www/sites/appname/deploy/*.zip -d "/var/www/sites/servizifv/deploy"
+## SEE BEFORE deploy_checkfile.sh HEADER
+rm -rf /var/www/sites/appname/deploy/*.deployed
+unzip /var/www/sites/appname/deploy/*.zip -d "/var/www/sites/appname/deploy"
 cd /var/www/sites/appname/webroot
 zip -r /var/www/sites/foo/backups/version-$(date +"%Y-%m-%d-%H-%M-%S").zip *
-rsync -vauX --remove-source-files /var/www/sites/foo/deploy/zipname/* /var/www/sites/foo/webroot
+rsync -vauX --remove-source-files /var/www/sites/foo/deploy/zipname/* /var/www/sites/appname/webroot
 rm -rf /var/www/sites/appname/deploy/zipname
 # Rename all *.txt to *.text
 for f in /var/www/sites/appname/deploy/*.zip; do
